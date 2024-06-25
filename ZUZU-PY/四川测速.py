56711f8e6239253100edafd85d2462e6c379b5cd
luffy9847 <95870943+pengdejiang@users.noreply.github.com>
2024-06-25 23:25:30 +0800

    Update 四川测速.py

--- ZUZU-PY/四川测速.py
+++ ZUZU-PY/四川测速.py
@@ -1,4 +1,4 @@
-﻿import requests
+import requests
 import time
 
 # 定义测速函数
@@ -15,22 +15,32 @@ def measure_speed(url):
         return None
 
 # 打开并读取“四川电信”文件
-with open("四川电信.txt", "r", encoding="utf-8") as file:
+with open("四川电信", "r", encoding="utf-8") as file:
     lines = file.readlines()
 
+# 创建一个集合来存储已经测试过的channel_url
+tested_urls = set()
+
 # 打开“四川测速结果”文件，准备写入结果
-with open("四川测速结果.txt", "w", encoding="utf-8") as output_file:
+with open("四川测速结果", "w", encoding="utf-8") as output_file:
     # 遍历每一行
     for line in lines:
         # 截取channel_name和channel_url
         channel_name = line.split(',')[0]
         channel_url = line.split(',')[1].split('$')[0].strip()
         
-        # 测速
-        speed = measure_speed(channel_url)
-        
-        # 判断速度并写入文件
-        if speed is not None and speed > 0.2:
-            output_file.write(f"{channel_name},{channel_url}${speed}\n")
-        elif speed is not None and speed <= 0.2:
-            print(f"删除行：{line.strip()}，速度 {speed:.2f} 秒")
+        # 检查channel_url是否已经测试过
+        if channel_url not in tested_urls:
+            # 测速
+            speed = measure_speed(channel_url)
+            # 将channel_url添加到已测试集合中
+            tested_urls.add(channel_url)
+            
+            # 判断速度并写入文件
+            if speed is not None and speed > 0.2:
+                output_file.write(line)
+            elif speed is not None and speed <= 0.2:
+                print(f"删除行：{line.strip()}，速度 {speed:.2f} 秒")
+        else:
+            # 如果channel_url已经测试过，则跳过测速
+            print(f"跳过已测试的channel_url：{channel_url}")
