220ecd58da46ca98b1d80de07f087881b2083df6
luffy9847 <95870943+pengdejiang@users.noreply.github.com>
2024-06-25 23:36:05 +0800

    Update 四川测速.py

--- ZUZU-PY/四川测速.py
+++ ZUZU-PY/四川测速.py
@@ -14,14 +14,15 @@ def measure_speed(url):
         print(f"请求错误: {e}")
         return None
 
-# 打开并读取“四川电信”文件
+# 打开并读取“四川电信.txt”文件
 with open("四川电信.txt", "r", encoding="utf-8") as file:
     lines = file.readlines()
 
-# 创建一个集合来存储已经测试过的channel_url
-tested_urls = set()
+# 创建两个集合来存储已经测试过的channel_ip_A和channel_ip_B
+tested_ips_A = set()
+tested_ips_B = set()
 
-# 打开“四川测速结果”文件，准备写入结果
+# 打开“四川测速结果.txt”文件，准备写入结果
 with open("四川测速结果.txt", "w", encoding="utf-8") as output_file:
     # 遍历每一行
     for line in lines:
@@ -29,12 +30,17 @@ with open("四川测速结果.txt", "w", encoding="utf-8") as output_file:
         channel_name = line.split(',')[0]
         channel_url = line.split(',')[1].split('$')[0].strip()
         
-        # 检查channel_url是否已经测试过
-        if channel_url not in tested_urls:
+        # 截取channel_ip_A和channel_ip_B
+        channel_ip_A = line.split(',')[1].split('/rtp')[0].strip()
+        channel_ip_B = line.split(',')[1].split('/udp')[0].strip()
+        
+        # 检查channel_ip_A和channel_ip_B是否已经测试过
+        if channel_ip_A not in tested_ips_A and channel_ip_B not in tested_ips_B:
             # 测速
             speed = measure_speed(channel_url)
-            # 将channel_url添加到已测试集合中
-            tested_urls.add(channel_url)
+            # 将channel_ip_A和channel_ip_B添加到已测试集合中
+            tested_ips_A.add(channel_ip_A)
+            tested_ips_B.add(channel_ip_B)
             
             # 判断速度并写入文件
             if speed is not None and speed > 0.2:
@@ -42,5 +48,5 @@ with open("四川测速结果.txt", "w", encoding="utf-8") as output_file:
             elif speed is not None and speed <= 0.2:
                 print(f"删除行：{line.strip()}，速度 {speed:.2f} 秒")
         else:
-            # 如果channel_url已经测试过，则跳过测速
-            print(f"跳过已测试的channel_url：{channel_url}")
+            # 如果channel_ip_A或channel_ip_B已经测试过，则跳过测速
+            print(f"跳过已测试的IP：{channel_ip_A}, {channel_ip_B}")
