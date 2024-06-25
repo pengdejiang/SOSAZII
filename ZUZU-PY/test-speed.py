12257f487897fb3262bcd75bb63d35fb6f90339d
luffy9847 <95870943+pengdejiang@users.noreply.github.com>
2024-06-25 17:53:07 +0800

    Update test-speed.py

--- ZUZU-PY/test-speed.py
+++ ZUZU-PY/test-speed.py
@@ -20,11 +20,12 @@ error_channels = []
 with open("四川电信.txt", 'r', encoding='utf-8') as file:
     lines = file.readlines()
     for line in lines:
-        line = line.strip()
-        if line:
-            channel_name, channel_url = line.split(',http')          
-            import re
-            if ',http' in channel_url:   # 提取所有,链接的行
+       if re.search(pattern, line) and line.count(',') == 1:  # 如果行中有任意关键字而且行内只有一个逗号
+         line = line.strip()
+         if line:
+             channel_name, channel_url = line.split(',')          
+             import re
+             if ',' in channel_url:   # 提取所有,链接的行
                 channels.append((channel_name, channel_url))
 
 # 定义工作线程函数
@@ -111,7 +112,7 @@ with open("四川电信测速后.txt", 'w', encoding='utf-8') as file:
     file.write('删除慢IP输出结果\n')
     for result in results:
         channel_name, channel_url, speed = result
-        if ',http' in channel_url: 
+        if 'http' in channel_url: 
             if channel_url in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
                     continue
